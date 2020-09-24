ce7db376254d7cf2490098d0a40ccbe805d3f5fe
Jared Kreppein <jaredkrep@gmail.com>
2020-09-24 09:56:53 -0600

    init commit

---
+++ config/initializers/appmap_remote_recording.rb
@@ -0,0 +1,7 @@
+require 'appmap/middleware/remote_recording'
+
+if Rails.env.development?
+  Rails.application.config.middleware.insert_after \
+    Rails::Rack::Logger,
+    AppMap::Middleware::RemoteRecording
+end

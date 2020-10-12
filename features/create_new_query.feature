dd578afcf2fb42b893f0ac9251c06fd799c60b16
Jared Kreppein <jaredkrep@gmail.com>
2020-10-12 09:32:14 -0600

    first cucumber test

--- features/create_new_query.feature
+++
@@ -1,7 +0,0 @@
-#@javascript
-Feature: Create a New Query
-
-    Scenario: User Creates a new Query
-        Given I am on the homepage
-        When I click on "New Query"
-        Then I should be taken to an html form to create the query
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,5 +3,8 @@ require 'selenium/webdriver'
 require 'capybara/cucumber'
 
 Capybara.default_max_wait_time = 30
-Capybara.default_driver = :selenium
+Capybara.default_driver = :selenium_chrome
 Capybara.run_server = false
+
+# What is proper way to do this?
+Capybara.app_host = 'http://localhost:3000'
